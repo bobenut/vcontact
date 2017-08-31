2ed3a864d82b50d6c02364d56173e56345ffbd94
bobenut <33755383@qq.com>
2017-08-31 11:51:54 +0800

    done

--- src/backend/gulpfile.js
+++ src/backend/gulpfile.js
@@ -11,100 +11,68 @@ gulp.task('default',sequence(
     'gulp_utilities_uglify',
     'gulp_views_copy',
     'gulp_test_copy',
-    'gulp_public_biz_css_copy',
-    'gulp_public_biz_imgs_copy',
-    'gulp_public_biz_js_uglify',
-    'gulp_public_framework_copy',
+    'gulp_public_copy',
     'gulp_node_modules_copy',
     'gulp_main_file_uglify',
     'gulp_main_file_copy'
 ));
 
 gulp.task('gulp_clean', function () {
-    return gulp.src('../bin/app', {read: false})
+    return gulp.src('../../bin/win/app', {read: false})
         .pipe(clean({force:true}));
 });
 
 gulp.task('gulp_controllers_uglify', function () {
     return gulp.src('controllers/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('../bin/app/controllers'));
+        .pipe(gulp.dest('../../bin/win/app/controllers'));
 });
 
 gulp.task('gulp_models_uglify', function () {
     return gulp.src('models/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('../bin/app/models'));
+        .pipe(gulp.dest('../../bin/win/app/models'));
 });
 
 gulp.task('gulp_routes_uglify', function () {
     return gulp.src('routes/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('../bin/app/routes'));
+        .pipe(gulp.dest('../../bin/win/app/routes'));
 });
 
 gulp.task('gulp_utilities_uglify', function () {
     return gulp.src('utilities/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('../bin/app/utilities'));
+        .pipe(gulp.dest('../../bin/win/app/utilities'));
 });
 
 gulp.task('gulp_views_copy', function () {
     return gulp.src('views/**/*')
-        .pipe(gulp.dest('../bin/app/views'));
+        .pipe(gulp.dest('../../bin/win/app/views'));
 });
 
 gulp.task('gulp_test_copy', function () {
     return gulp.src('test/**/*')
-        .pipe(gulp.dest('../bin/app/test'));
+        .pipe(gulp.dest('../../bin/win/app/test'));
 });
 
-gulp.task('gulp_public_biz_css_copy', function () {
-    return gulp.src('public/biz/css/**/*')
-        .pipe(gulp.dest('../bin/app/public/biz/css'));
-});
-
-gulp.task('gulp_public_biz_imgs_copy', function () {
-    return gulp.src('public/biz/imgs/**/*')
-        .pipe(gulp.dest('../bin/app/public/biz/imgs'));
-});
-
-gulp.task('gulp_public_biz_js_uglify', function () {
-    return gulp.src('public/biz/js/**/*')
-        .pipe(uglify({
-            mangle: {
-                except: [
-                    '$scope',
-                    '$http',
-                    '$modal',
-                    '$log',
-                    '$modalInstance',
-                    'contactCreationOrEditFactory',
-                    'contactCreationOrEditFactory',
-                    'editableContact'
-                ]
-            }
-        }))
-        .pipe(gulp.dest('../bin/app/public/biz/js'));
-});
-
-gulp.task('gulp_public_framework_copy', function () {
-    return gulp.src('public/framework/**/*')
-        .pipe(gulp.dest('../bin/app/public/framework'));
+gulp.task('gulp_public_copy', function () {
+    return gulp.src('public/**/*')
+        .pipe(gulp.dest('../../bin/win/app/public'));
 });
 
 gulp.task('gulp_node_modules_copy', function () {
     return gulp.src('node_modules/**/*')
-        .pipe(gulp.dest('../bin/app/node_modules'));
+        .pipe(gulp.dest('../../bin/win/app/node_modules'));
 });
 
 gulp.task('gulp_main_file_uglify', function () {
     return gulp.src(['app.js', 'www'])
         .pipe(uglify())
-        .pipe(gulp.dest('../bin/app'));
+        .pipe(gulp.dest('../../bin/win/app'));
 });
 
 gulp.task('gulp_main_file_copy', function () {
     return gulp.src(['package.json'])
-        .pipe(gulp.dest('../bin/app'));
+        .pipe(gulp.dest('../../bin/win/app'));
 });
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('***************come in**********')
-  fs.stat('../public/index.html', (err, stats) => {
+  fs.stat('../public/index.html', function(err, stats) {
     if(err){
       throw err;
     }
 
     res.setHeader('Content-Type','text/html;charset=utf-8');
-    res.setHeader('Content-length', stats.size)
+    res.setHeader('Content-length', stats.size);
     res.sendfile('../public/index.html');
   });
 });
